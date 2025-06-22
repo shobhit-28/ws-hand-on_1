9ecddbcd06ecdb31befff15ef8a131504a5772c5
shobhit-28 <shobhitraj34@gmail.com>
2025-06-22 17:21:30 +0530

    some initial config done

--- client/socket_practice/src/app/app.component.ts
+++ client/socket_practice/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [MatSlideToggleModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
