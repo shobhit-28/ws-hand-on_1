35c7c8464018be0712275bf9757cdf637e90d100
shobhit-28 <shobhitraj34@gmail.com>
2025-07-03 23:31:09 +0530

    centralised routes

--- server/routes/central.routes.js
+++ server/routes/central.routes.js
@@ -10,14 +10,10 @@ const __dirname = path.dirname(__filename)
 
 const routerFiles = fs.readdirSync(__dirname).filter(file => file !== 'central.routes.js')
 
-console.log(routerFiles)
-
 for (const file of routerFiles) {
     const route = await import(`./${file}`)
     const routeName = file.replace('.routes.js', '').toLowerCase();
-    console.log(route)
-    console.log('routeName', routeName)
     router.use(`/${routeName}`, route.default)
 }
-// console.log(router)
+
 export default router
