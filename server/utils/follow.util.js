50e7a223105b3503a28b3ac488e47881c2cdf9d0
Shobhit Raj <Shobhit.Raj@ril.com>
2025-07-08 18:26:33 +0530

    added follow controller dto remaining

--- server/utils/follow.util.js
+++ server/utils/follow.util.js
@@ -1,20 +1,11 @@
 import Follow from "../models/follow.model.js"
-import { AppError } from "./appError.js";
 
 export const isAlreadyFollowing = async (follower, following) => {
-    try {
-        const existing = await Follow.findOne({ follower, following })
-        return !!existing;        
-    } catch (error) {
-        throw new AppError(error)
-    }
+    const existing = await Follow.findOne({ follower, following })
+    return !!existing;
 }
 
 export const isGettingFollowed = async (follower, following) => {
-    try {
-        const isGettingFollowed = await Follow.findOne({ follower: following, following: follower })
-        return !!isGettingFollowed        
-    } catch (error) {
-        throw new AppError(error)
-    }
+    const isGettingFollowed = await Follow.findOne({ follower: following, following: follower })
+    return !!isGettingFollowed
 }
