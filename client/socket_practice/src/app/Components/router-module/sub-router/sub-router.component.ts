8dbd35af80cc91b76128d04cbaf08247a3fb7b35
Shobhit Raj <Shobhit.Raj@ril.com>
2025-07-11 12:06:39 +0530

    user details added in side nav

--- client/socket_practice/src/app/Components/router-module/sub-router/sub-router.component.ts
+++ client/socket_practice/src/app/Components/router-module/sub-router/sub-router.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { ChromeDataTransactionService } from '../../../services/chromeDataTransaction/chrome-data-transaction.service';
 
 @Component({
   selector: 'raj-chat-sub-router',
@@ -19,6 +20,8 @@ export class SubRouterComponent {
     { path: 'chat', name: 'Chat', icon: 'chat' }
   ]);
 
+  private dataService = inject(ChromeDataTransactionService)
+
   userDetails: {
     userName: string,
     userEmail: string,
@@ -27,4 +30,8 @@ export class SubRouterComponent {
     userName: 'Shobhit Raj',
     userEmail: 's@gm.co'
   }
+
+  getUserDetails = () => this.dataService.getCookies('user')
+
+  getImgOfUser = () => this.getUserDetails()?.profile_pic || "/assets/profile/empty_profile_male.svg"
 }
